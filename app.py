d1d4d962372d81163196976e49367fa505fa4940
Fred <efrediii@hotmail.com>
2019-10-03 21:01:42 -0500

    Copyright © 2019 by Castrow, L.L.C., Version 0.0.2

--- app.py
+++ app.py
@@ -1,18 +1,75 @@
-from flask import Flask, jsonify
+#import the usual suspects
+#%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+#import Python SQL toolkit and Object Relational Mapper
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, text
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#populate data frames
+results = session.query(Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+stations_df = pd.DataFrame(results, columns=['id', 'station', 'name', 'latitude', 'longitude', 'elevation'])
+
+results = session.query(Measurement.id, Measurement.station, Measurement.date, Measurement.prcp, Measurement.tobs).all()
+measurements_df = pd.DataFrame(results, columns=['id', 'station', 'date', 'prcp', 'tobs'])
+
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
 # list available routes
-    return "/api/v1.0/precipatation, /api/v1.0/stations, /api/v1.0/tobs, /api/v1.0/<start>, /api/v1.0/<start>/<end>"
+    return "Routes:<hr>/api/v1.0/precipatation<br>/api/v1.0/stations<br>/api/v1.0/tobs<br>/api/v1.0/start_date<br>/api/v1.0/start_date/end_date"
 
 @app.route("/api/v1.0/precipatation")
-    return "stations"
+def precipatation():
+    d = {}
+    for i in measurements_df['id']:
+        i -= 1
+        d[i] = {
+                'id': int(measurements_df['id'][i]),
+                'station': measurements_df['station'][i],
+                'date': measurements_df['date'][i],
+                'prcp': measurements_df['prcp'][i],
+                'tobs': measurements_df['tobs'][i]
+        }
+    return jsonify(d)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    return "stations"
+    d = {}
+    for i in stations_df['id']:
+        i -= 1
+        d[i] = {
+                'id': int(stations_df['id'][i]),
+                'station': stations_df['station'][i],
+                'name': stations_df['name'][i],
+                'latitude': stations_df['latitude'][i],
+                'longitude': stations_df['longitude'][i],
+                'elevation': stations_df['elevation'][i]
+        }
+    return jsonify(d)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
